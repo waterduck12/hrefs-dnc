03f46a761ad71d3d3af84fc57db5c593fad6ab9c
Hyeonmin Koo <hyeon6423@naver.com>
2022-04-12 16:36:37 +0900

    v0.2.2

--- dncproject/dncproject/wsgi.py
+++ dncproject/dncproject/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dncproject.settings')
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
